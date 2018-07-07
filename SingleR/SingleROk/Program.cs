55128e1d4ca2f443e5c4f51af7a5dcbc58200547
Jenny <fantazmit@gmail.com>
2018-07-07 17:22:39 +0200

    Single responsability. Clsoe #1.

---
+++ SingleR/SingleROk/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//[assembly: log4net.Config.XmlConfigurator(Watch = true)] <- Moved to AssemblyInfo.cs
+
+namespace SingleROk
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            log.Error("this is an error message.");
+
+            Console.ReadLine();
+        }
+    }
+}
